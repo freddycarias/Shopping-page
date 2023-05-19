4dda6d69137c4b3fe8782b99fa1c7fe670172411
Rene Vasquez <freddyrenecariasvasquez@gmail.com>
2023-05-19 09:52:42 -0600

    I create the footer of my website

---
+++ src/layout/Footer/Footer.tsx
@@ -0,0 +1,36 @@
+import ContactUs from "./ContactUs";
+import '../../styles/ContactUs.css'
+export default function Footer() {
+  return (
+    <footer className="nav-footer-vertical-column">
+      <hr className="hr-footer"/>
+      <div className="container px-4 text-center">
+        <div className="row gx-5">
+          <div className="col">
+            <div className="p-3">
+              <ContactUs />
+            </div>
+          </div>
+          <div className="col">
+            <div className="p-3">
+              <ul>
+                <li>Customer Support:</li>
+                <li>(XXX)-XXXX-XXXX</li>
+              </ul>
+            </div>
+          </div>
+          <div className="col">
+            <div className="p-3">
+              <ul>
+                <li>Email:</li>
+                <li>page@gmail.com</li>
+                <li>Location:</li>
+                <li>Xa Avenida XX-XXXX Zone XX</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
--- src/pages/Home/Store.tsx
+++ src/pages/Home/Store.tsx
@@ -1,9 +1,11 @@
+import Footer from "../../layout/Footer/Footer";
 import Header from "../../layout/Header/Header";
 
 export default function Store() {
   return (
     <>
       <Header/>
+      <Footer/>
     </>
   )
 }
