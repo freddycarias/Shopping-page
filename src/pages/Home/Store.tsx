6ec85b9d1cfb50bc9b8ddf2a77cc2fbfd358f1d6
Rene Vasquez <freddyrenecariasvasquez@gmail.com>
2023-05-24 13:17:37 -0600

    I added ProductListRenderer in the Store

--- src/pages/Home/Store.tsx
+++ src/pages/Home/Store.tsx
@@ -1,10 +1,12 @@
-import Footer from "../../layout/Footer/Footer";
 import Header from "../../layout/Header/Header";
+import Footer from "../../layout/Footer/Footer";
+import { ProductListRenderer } from "../../components/Products/productListRenderer.component";
 
 export default function Store() {
   return (
     <>
       <Header/>
+      <ProductListRenderer/>
       <Footer/>
     </>
   )
