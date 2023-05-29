4ee4cf1769c3cb2380ec19211cbaf8753775b7fb
Rene Vasquez <freddyrenecariasvasquez@gmail.com>
2023-05-29 09:29:55 -0600

    And I added in store file

--- src/pages/Home/Store.tsx
+++ src/pages/Home/Store.tsx
@@ -1,13 +1,13 @@
 import Header from "../../layout/Header/Header";
 import Footer from "../../layout/Footer/Footer";
-import { ProductListRenderer } from "../../components/Products/productListRenderer.component";
+import ListProducts from "../../components/firebaseProducts/ProductsList.component";
 
 export default function Store() {
   return (
     <>
-      <Header/>
-      <ProductListRenderer/>
-      <Footer/>
+      <Header />
+      <ListProducts />
+      <Footer />
     </>
-  )
+  );
 }
