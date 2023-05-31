9bd7ac6dc430e6ac526bfe790356b265b4f5be56
Rene Vasquez <freddyrenecariasvasquez@gmail.com>
2023-05-31 14:16:39 -0600

    I added ShowFullProduct in the routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Store from "../pages/Home/Store";
 import SignIn from "../pages/SignInAndRegister/SignIn";
 import Register from "../pages/SignInAndRegister/Register";
+import ShowFullProduct from "../pages/ShowFullProduct/ShowFullProduct";
 
 export default function PagesRoutes() {
   return (
@@ -10,6 +11,7 @@ export default function PagesRoutes() {
         <Route path="/" element={<Store />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/show-full-product/:id" element={<ShowFullProduct/>}/>
       </Routes>
     </BrowserRouter>
   );
