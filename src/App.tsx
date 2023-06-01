884b08f241dc271eeafcc9aa1f5b4462030cc966
Rene Vasquez <freddyrenecariasvasquez@gmail.com>
2023-06-01 10:58:13 -0600

    I use the safe way to take care of my routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import PagesRoutes from "./routes/routes";
+import { Route, Routes } from "react-router-dom";
+import SignIn from "./components/SignIn";
+import Register from "./components/Register";
+import Store from "./pages/Home/Store";
+import ShowFullProduct from "./pages/ShowFullProduct/ShowFullProduct";
 import "./styles/App.css";
 
 function App() {
-  return <PagesRoutes />;
+  return(
+    <Routes>
+      <Route path="/" element={<Store />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/show-full-product/:id" element={<ShowFullProduct />} />
+    </Routes>
+  );
 }
 
 export default App;
