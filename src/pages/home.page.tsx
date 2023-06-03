5a6ee934adfca937119ff97da6a5ff2eb6ca63f4
Rene Vasquez <freddyrenecariasvasquez@gmail.com>
2023-06-03 10:31:40 -0600

    You have to be specific with the names

---
+++ src/pages/home.page.tsx
@@ -0,0 +1,21 @@
+import Header from "../layout/Header/Header";
+import Footer from "../layout/Footer/Footer";
+import ListProducts from "../components/Product/ProductsList.component";
+
+export default function HomePage() {
+  return (
+    <>
+      <div className="contaner">
+        <div className="row">
+          <Header />
+        </div>
+        <div className="row">
+          <ListProducts />
+        </div>
+        <div className="row">
+        <Footer />
+        </div>
+      </div>
+    </>
+  );
+}
