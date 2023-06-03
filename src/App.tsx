0622d8766f521455a1965f531db8a0e56a756b44
Rene Vasquez <freddyrenecariasvasquez@gmail.com>
2023-06-03 11:03:21 -0600

    You have to be specific with the names. here  I change the names

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { Route, Routes } from "react-router-dom";
-import SignIn from "./components/SignIn";
-import Register from "./components/Register";
-import Store from "./pages/Home/Store";
-import ProductDetail from "./components/ProductDetail.component";
+import SignIn from "./components/Auth/SignIn";
+import Register from "./components/Auth/Register";
+import HomePage from "./pages/home.page";
+import ProductDetailPage from "./pages/product-detail.page";
 import "./styles/App.css";
 
 function App() {
   return(
     <Routes>
-      <Route path="/" element={<Store />} />
+      <Route path="/" element={<HomePage />} />
       <Route path="/register" element={<Register />} />
       <Route path="/signin" element={<SignIn />} />
-      <Route path="/show-full-product/:id" element={<ProductDetail />} />
+      <Route path="/show-full-product/:id" element={<ProductDetailPage />} />
     </Routes>
   );
 }
