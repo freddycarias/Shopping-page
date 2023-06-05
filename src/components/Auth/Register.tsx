56c30328922f8ee8e355d81b05f63be323f4c378
Rene Vasquez <freddyrenecariasvasquez@gmail.com>
2023-06-05 10:28:41 -0600

    I added the authService in register and sign In

--- src/components/Auth/Register.tsx
+++ src/components/Auth/Register.tsx
@@ -1,88 +1,25 @@
-import { auth } from "../../services/firebase";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { createUserWithEmailAndPassword } from "firebase/auth";
 import "../../styles/Register.css";
+import { authService } from "../../services/auth.service";
+import SignInAndRegister from "./SignInAndRegister.component";
 
 export default function Register() {
-  const navigate = useNavigate();
-  const [registerEmail, setRegisterEmail] = useState("");
-  const [registerPassword, setRegisterPassword] = useState("");
-  const [loginError, setLoginError] = useState("");
 
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  
   const handleRegister = async () => {
     try {
-      const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        registerEmail,
-        registerPassword
-      );
-      const newUser = userCredential.user;
-      console.log(newUser);
-      navigate("/");
-    } catch (error: any) {
-      console.log(error.message);
-      if (error.code === "auth/invalid-email") {
-        setLoginError("Correo Invalido");
-      } else {
-        setLoginError("");
-      }
+      await authService.registerUser({email, password })
+    } catch (error) {
+      console.error('Error al registrar usuario:', error);
     }
   };
 
+  const handles = {
+    handleRegister
+  };
   return (
-    <>
-      <div className="company-logo-in-register">
-        <a className="navbar-brand" href="/">
-          <img className="company-logo-img" />
-        </a>
-      </div>
-      <div className="container text-center border-register">
-        <form className="register-form">
-          <div className="row mb-3">
-            <label
-              htmlFor="inputEmail3"
-              className="col-sm-2 col-form-label email-label"
-            >
-              Email
-            </label>
-            <div className="col-sm-10">
-              <input
-                type="email"
-                className="form-control email-input"
-                id="inputEmail3"
-                placeholder="Email..."
-                required
-                value={registerEmail}
-                onChange={(event) => setRegisterEmail(event.target.value)}
-              />
-            </div>
-          </div>
-          <div className="row mb-3">
-            <label
-              htmlFor="inputPassword3"
-              className="col-sm-2 col-form-label password-label"
-            >
-              Password
-            </label>
-            <div className="col-sm-10">
-              <input
-                type="password"
-                className="form-control password-input"
-                id="inputPassword3"
-                placeholder="Password..."
-                required
-                value={registerPassword}
-                onChange={(event) => setRegisterPassword(event.target.value)}
-              />
-            </div>
-          </div>
-        </form>
-        {loginError && <p>{loginError}</p>}
-        <button onClick={handleRegister} className="btn btn-primary">
-          Create User
-        </button>
-      </div>
-    </>
-  );
+    <SignInAndRegister email={email} setEmail={setEmail} password={password} setPassword={setPassword} handles={handles} register={true}/>
+    );
 }
--- src/components/Auth/SignIn.tsx
+++ src/components/Auth/SignIn.tsx
@@ -1,135 +1,38 @@
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import {
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-  signOut,
-  User,
-} from "firebase/auth";
-import { auth } from "../../services/firebase";
-
+import { useState } from "react";
+import { authService } from "../../services/auth.service";
 import "../../styles/SignIn.css";
+import SignInAndRegister from "./SignInAndRegister.component";
 
 export default function SignIn() {
-  const [loginEmail, setLoginEmail] = useState("");
-  const [loginPassword, setLoginPassword] = useState("");
-  const [user, setUser] = useState<User | null>(null);
-  const [loading, setLoading] = useState(true);
-  const [loginError, setLoginError] = useState(null);
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-      setLoading(false);
-    });
-
-    return () => unsubscribe();
-  }, []);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleLogin = async () => {
     try {
-      const userCredential = await signInWithEmailAndPassword(
-        auth,
-        loginEmail,
-        loginPassword
-      );
-      const loggedInUser = userCredential.user;
-      console.log(loggedInUser);
-
-      navigate("/"); // Redirige al usuario a la página de inicio ("/")
-    } catch (error: any) {
-      setLoginError(error.message);
+      await authService.loginUser({email, password});
+      // Aquí puedes realizar acciones adicionales después de iniciar sesión exitosamente
+    } catch (error) {
+      // Manejo de errores al iniciar sesión
+      console.error('Error al iniciar sesión:', error);
     }
   };
 
   const handleLogout = async () => {
     try {
-      await signOut(auth);
-    } catch (error: any) {
-      console.log(error.message);
+      await authService.logoutUser();
+      // Aquí puedes realizar acciones adicionales después de cerrar sesión exitosamente
+    } catch (error) {
+      // Manejo de errores al cerrar sesión
+      console.error('Error al cerrar sesión:', error);
     }
   };
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  const handles = {
+    handleLogin,
+    handleLogout,
+  };
+
   return (
-    <>
-      <div className="company-logo-in-register">
-        <a className="navbar-brand" href="/">
-          <img className="company-logo-img" />
-        </a>
-      </div>
-      <div className="container text-center border sign-in">
-        <form className="sign-in-form" onSubmit={() => navigate("/")}>
-          <div className="row mb-3">
-            <label
-              htmlFor="inputEmail3"
-              className="col-sm-2 col-form-label email-label"
-            >
-              Email
-            </label>
-            <div className="col-sm-10">
-              <input
-                type="email"
-                className="form-control email-input"
-                id="inputEmail3"
-                placeholder="Email..."
-                value={loginEmail}
-                onChange={(event) => setLoginEmail(event.target.value)}
-                required
-              />
-            </div>
-          </div>
-          <div className="row mb-3">
-            <label
-              htmlFor="inputPassword3"
-              className="col-sm-2 col-form-label password-label"
-            >
-              Password
-            </label>
-            <div className="col-sm-10">
-              <input
-                type="password"
-                className="form-control password-input"
-                id="inputPassword3"
-                placeholder="Password..."
-                value={loginPassword}
-                onChange={(event) => setLoginPassword(event.target.value)}
-                required
-              />
-            </div>
-          </div>
-          <div className="sign-in-button">
-            <button
-              type="submit"
-              className="btn btn-warning"
-              onClick={handleLogin}
-            >
-              Sign in
-            </button>
-            {loginError && <p>{loginError}</p>}
-          </div>
-        </form>
-
-        <h4>User Logged In:</h4>
-        {user ? <p>{user.email}</p> : <p>No user logged in</p>}
-
-        <button onClick={handleLogout} className="btn btn-danger">
-          Sign Out
-        </button>
-
-        <div className="register-button">
-          <button
-            className="btn btn-secondary"
-            onClick={() => navigate("/register")}
-          >
-            Create Your account in Online shop
-          </button>
-        </div>
-      </div>
-    </>
+    <SignInAndRegister email={email} setEmail={setEmail} password={password} setPassword={setPassword} handles={handles} register={false}/>
   );
 }
