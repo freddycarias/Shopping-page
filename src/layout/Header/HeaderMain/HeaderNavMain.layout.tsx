dc2f61dc868cd8f2e77f6404849ee9baf28f6c9b
Rene Vasquez <freddyrenecariasvasquez@gmail.com>
2023-06-05 10:38:46 -0600

    I improve the code

--- src/layout/Header/HeaderMain/HeaderNavMain.layout.tsx
+++ src/layout/Header/HeaderMain/HeaderNavMain.layout.tsx
@@ -3,24 +3,33 @@ import HeaderHmenuCanvas from "./HeaderHmenuCanvas";
 import "../../../styles/HeaderNavMain.css";
 
 export default function HeaderNavMainLayout() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <div className="nav-main">
-      <nav className="navbar">
-        <div className="container-fluid sub-header">
-          <a
-            className="nav-hambuger-menu"
-            data-bs-toggle="offcanvas"
-            href="#offcanvasExample"
-            role="button"
-            aria-controls="offcanvasExample"
-          >
-            =
-          </a>
-          <HeaderHmenuCanvas/>          
-          <button className="custom-service-button" onClick={() => navigate("/customer-service")}>Custom Service</button>
+      <div className="row justify-content-center">
+        <div className="col-6 mb-3" style={{ textAlign: "left"}}>
+          <div>
+            <button
+              className="btn btn-primary hamburger-menu"
+              type="button"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasExample"
+              aria-controls="offcanvasExample"
+            >
+               ≡
+            </button>
+            <HeaderHmenuCanvas />
+          </div>
         </div>
-      </nav>
+        <div className="col-6 mb-3 " style={{ textAlign: "right"}}>
+          <button
+            className="btn btn-primary custom-service-button"
+            onClick={() => navigate("/custom-service")}
+          >
+            Custom Service
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
