72a2cded9f262e7f67fd6f4d56466918bc5f4f69
Rene Vasquez <freddyrenecariasvasquez@gmail.com>
2023-06-05 16:16:41 -0600

    I added the route

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import SignIn from "./components/Auth/SignIn";
 import Register from "./components/Auth/Register";
 import HomePage from "./pages/home.page";
 import ProductDetailPage from "./pages/product-detail.page";
+import { CustomerServiceComponent } from "./components/CustomerService/CostumerService.component";
 import "./styles/App.css";
 
 function App() {
   return(
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/register" element={<Register />} />
+      <Route path="/register"  element={<Register />} />
       <Route path="/signin" element={<SignIn />} />
+      <Route path="customer-service" element={<CustomerServiceComponent/>}/>
       <Route path="/show-full-product/:id" element={<ProductDetailPage />} />
     </Routes>
   );
