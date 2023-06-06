9bc15ae252e18918bf28cc62dbf98d2197728fd8
Rene Vasquez <freddyrenecariasvasquez@gmail.com>
2023-06-06 11:36:26 -0600

    I improve the code , don't repeat yourself

--- src/components/CustomerService/CostumerService.component.tsx
+++ src/components/CustomerService/CostumerService.component.tsx
@@ -1,24 +1,33 @@
-import Footer from "../../layout/Footer/Footer"
-import Header from "../../layout/Header/Header"
+import Footer from "../../layout/Footer/Footer";
+import Header from "../../layout/Header/Header";
 import ServiceComponent from "./Service.component";
 
 export const CustomerServiceComponent = () => {
   return (
     <>
       <Header />
-      <div className="row row-cols-1 row-cols-md-3 g-4" style={{marginBottom: "75px"}}>
-        <ServiceComponent title={"A delivery, order or return"}/>
-        <ServiceComponent title={"Online"}/>
-        <ServiceComponent title={"Payment, charges or gift cards"}/>
-        <ServiceComponent title={"Address, security & privacy"}/>
-        <ServiceComponent title={"Memberships, subscriptions or communications"}/>
-        <ServiceComponent title={"Kindle, Fire, Alexa or Other Amazon Devices"}/>
-        <ServiceComponent title={"eBooks, Prime Videos or Music"}/>
-        <ServiceComponent title={"Accessibility"}/>
-        <ServiceComponent title={"Something else"}/>
-        <ServiceComponent title={"Login & password"}/>
+      <div
+        className="row row-cols-1 row-cols-md-3 g-4"
+        style={{ marginBottom: "75px" }}
+      >
+        {services.map((service) => (
+          <ServiceComponent title={service.title} key={service.id} />
+        ))}
       </div>
       <Footer />
     </>
   );
-}
+};
+
+const services = [
+  { id: 1, title: "A delivery, order or return" },
+  { id: 2, title: "Online" },
+  { id: 3, title: "Payment, charges or gift cards" },
+  { id: 4, title: "Address, security & privacy" },
+  { id: 5, title: "Memberships, subscriptions or communications" },
+  { id: 6, title: "Kindle, Fire, Alexa or Other Amazon Devices" },
+  { id: 7, title: "eBooks, Prime Videos or Music" },
+  { id: 8, title: "Accessibility" },
+  { id: 9, title: "Something else" },
+  { id: 10, title: "Login & password" },
+];
