42df4393f79b8701278cbfc25156c0360961b214
Rene Vasquez <freddyrenecariasvasquez@gmail.com>
2023-06-07 16:42:40 -0600

    I create the sevice getByCategory

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -7,31 +7,31 @@ export const productService = {
     try {
       //hace referencia a Products de fireStore
       const dataCollectionRef = collection(database, "Products");
-      //obtiene los documentos de Products y los almacena 
+      //obtiene los documentos de Products y los almacena
       const dataDocuments = await getDocs(dataCollectionRef);
-      
+
       // let products: Product[] = [];
       // for (let index = 0; index < dataDocuments.docs.length; index++) {
       //   const doc = dataDocuments.docs[index]; //a // b //c 1
 
       //   const product = doc.data() as Product;
       //   product.id = doc.id;
-      //   products.push(product); 
+      //   products.push(product);
       // }
-      
+
       // llamamos los documentos con .docs ,despues con el . map itero sobre cada documento y
-      //lo guardo en el parametro doc 
+      //lo guardo en el parametro doc
       const data = dataDocuments.docs.map((doc) => {
         //se extrae la informacion con doc.data() y
         //devuelve un objeto que contiene los campos y valores del documento.
         //se asume que la información extraída del documento se ajusta al tipo Product
         const product = doc.data() as Product;
-        //se asigna el id del documento a la propiedad id del objeto product. 
+        //se asigna el id del documento a la propiedad id del objeto product.
         //Esto se hace mediante product.id = doc.id
         //doc.id contiene el ID único del documento
         product.id = doc.id;
-        //e devuelve el objeto product en cada iteración del map, 
-        //lo que resulta en un nuevo arreglo llamado data que contiene todos los objetos Product 
+        //e devuelve el objeto product en cada iteración del map,
+        //lo que resulta en un nuevo arreglo llamado data que contiene todos los objetos Product
         //obtenidos de los documentos
         return product;
       });
@@ -53,6 +53,23 @@ export const productService = {
       throw new Error(`product ${id} doesn't exists`);
     } catch (error) {
       throw new Error("Error fetching data from Firestore");
+    }
+  },
+  getByCategory: async (category: string) => {
+    try {
+      const dataCollectionRef = collection(database, "Products");
+      const querySnapshot = await getDocs(dataCollectionRef);
+
+      const data = querySnapshot.docs
+        .filter((doc) => doc.data().productType === category)
+        .map((doc) => {
+          const product = doc.data() as Product;
+          product.id = doc.id;
+          return product;
+        });
+      return data;
+    } catch (error) {
+      throw new Error ("Error fetching data from Firestore")
     }
   },
 };
